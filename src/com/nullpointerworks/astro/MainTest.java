d5d0490ee68e2b46edaae8e8af6c081df0aed300
Nullpointer <38044500+NullpointerWorks@users.noreply.github.com>
2022-01-29 00:56:12 +0100

    Update MainTest.java

--- src/com/nullpointerworks/astro/MainTest.java
+++ src/com/nullpointerworks/astro/MainTest.java
@@ -44,7 +44,11 @@ public class MainTest
 		 */
 		IMeasurement scopeDiameter 		= new Measurement(70.0, Unit.MILLI);
 		IMeasurement scopeFocalLength 	= new Measurement(420.0, Unit.MILLI);
-		ITelescope scope 				= new CustomTelescope(scopeDiameter, scopeFocalLength);
+		ITelescope scopeTSO70ED			= new CustomTelescope(scopeDiameter, scopeFocalLength);
+		
+		IMeasurement pdsDiameter 		= new Measurement(130.0, Unit.MILLI);
+		IMeasurement pdsFocalLength 	= new Measurement(650.0, Unit.MILLI);
+		ITelescope scope130PDS			= new CustomTelescope(pdsDiameter, pdsFocalLength);
 		
 		/*
 		 * 
@@ -55,11 +59,19 @@ public class MainTest
 		ISensor my1100DSLR			= new CustomSensor("Canon 1100D");
 		my1100DSLR.setPixelSize( new Measurement(5.2, Unit.MICRO) );
 		
+		ISensor myAtik320E			= new CustomSensor("Atil 320E Color");
+		myAtik320E.setPixelSize( new Measurement(4.4, Unit.MICRO) );
+		
+		/*
+		 * test the suitability of the sensor to the telescope
+		 */
+		testSuitibility(scopeTSO70ED, myAtik320E);
+		
 		
 		
 		/*
 		 * get some details
-		 */
+		 *
 		IMeasurement focalRatio = scope.getFocalRatio();
 		System.out.println("telescope's focal ratio: "+focalRatio.getValue());
 		
@@ -68,25 +80,57 @@ public class MainTest
 		
 		IMeasurement trueFOV = scope.getTrueFieldOfView(eyepiece);
 		System.out.println("the true field of view with the eyepiece is: "+trueFOV.getValue());
-		
+		//*/
+	}
+	
+
+	// If the resolving power of your telescope is better then the pixel resolution of the sensor
+	// then your sensor is not able to catch the detail provided by the telescope. 
+	// This means the sensor is undersampling.
+	
+	// If however the pixel resolution of the sensor is better than the resolving power of the telescope
+	// the image may look blurry when viewed at normal resolution. 
+	// This means the sensor is oversampling.
+	
+	// The ideal situation would be to match the resolving power and pixel resolution.
+	
+	void testSuitibility(ITelescope scope, ISensor sensor)
+	{
 		/*
 		 * find telescope's resolving power
 		 */
 		IMeasurement wavelength = new Measurement(650.0, Unit.NANO);
 		IMeasurement resolve = scope.getResolvingPower(wavelength);
 		resolve.setUnit(Unit.ARCSECOND);
-		System.out.println("the minimum resolving power is: "+resolve.getValue() +" "+resolve.getUnit());
+		System.out.println("minimum resolving power is: "+resolve.getValue() +" "+resolve.getUnit());
 		
 		/*
-		 * find sensor and telescope suitability
+		 * find sensor's pixel resolution
 		 */
-		IMeasurement pixRes = my350DSLR.getPixelResolution( scope.getFocalLength() );
-		System.out.println("Sensor resolution "+pixRes.getValue() + " "+pixRes.getUnit() +" per pixel");
+		IMeasurement pixRes = sensor.getPixelResolution( scope.getFocalLength() );
+		System.out.println("Sensor resolution is:       "+pixRes.getValue() + " "+pixRes.getUnit() +" per pixel");
 		
+		/*
+		 * compare the two
+		 */
+		double scopeResolve = resolve.getValue();
+		double pixelResolution = pixRes.getValue();
+		double delta = pixelResolution - scopeResolve;
+		double margin = 0.2; // in arcsecond
 		
-		
-		
+		if (delta < -margin)
+		{
+			System.out.println("This setup is oversampling.");
+		}
+		else
+		if (delta > margin)
+		{
+			System.out.println("This setup is undersampling.");
+		}
+		else
+		{
+			System.out.println("This setup is pretty good!");
+		}
 	}
-	
 	
 }
